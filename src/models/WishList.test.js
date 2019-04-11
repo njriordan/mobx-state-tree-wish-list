961976c79a0994386a86adc572ded03755e3f714
Niall Riordan <nriordan.dev@gmail.com>
2019-04-11 11:08:10 +0200

    Lesson 2: Attach behaviour to MST models using Actions

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -9,9 +9,12 @@ it("can create an instance of a model", () => {
   })
 
   expect(item.price).toBe(28.5)
+
+  item.changeName("Narnia")
+  expect(item.name).toBe("Narnia")
 })
 
-it ("can create a wishlist", () => {
+it("can create a wishlist", () => {
   const list = WishList.create({
     items: [{
       name: "Chronicles of Narnia",
@@ -21,4 +24,18 @@ it ("can create a wishlist", () => {
   })
 
   expect(list.items.length).toBe(1)
+})
+
+
+it("can add new items", () => {
+  const list = WishList.create()
+  list.add(WishListItem.create({
+    name: "Pieds d'argile",
+    price: 22.29
+  }))
+
+  expect(list.items.length).toBe(1)
+  expect(list.items[0].name).toBe("Pieds d'argile")
+  list.items[0].changeName("Au guet !")
+  expect(list.items[0].name).toBe("Au guet !")
 })
