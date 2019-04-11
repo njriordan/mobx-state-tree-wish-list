6251d501a0fd4252f5ff1137e8b3c426d142dfdb
Niall Riordan <nriordan.dev@gmail.com>
2019-04-11 14:57:46 +0200

    Lesson 9: Store state in local storage

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+import { onSnapshot } from "mobx-state-tree"
+
 import { WishList } from './models/WishList'
 
-const wishList = WishList.create({
+let initialState = {
   items: [
     {
       name: "Pyramides",
@@ -18,6 +20,19 @@ const wishList = WishList.create({
       image: "https://m.media-amazon.com/images/I/71P8zySaR8L._AC_UL872_QL65_.jpg"
     }
   ]
+}
+
+if (localStorage.getItem('wishListApp')) {
+  const json = JSON.parse(localStorage.getItem('wishListApp'))
+  if (WishList.is(json)) {
+    initialState = json
+  }
+}
+
+const wishList = WishList.create(initialState)
+
+onSnapshot(wishList, snapshot => {
+  localStorage.setItem('wishListApp', JSON.stringify(snapshot))
 })
 
 ReactDOM.render(<App  wishList={wishList} />, document.getElementById('root'));
